78d8c625db6d2ac4663d3cf1abb443c0546a0660
Tay <tayd93@hotmail.com>
2019-06-04 09:22:56 -0600

    Writes attributes test

--- lib/photograph.rb
+++ lib/photograph.rb
@@ -1,9 +1,13 @@
 require 'pry'
 
 class Photograph
-  attr_reader :attributes
+  attr_reader :attributes, :id, :name, :artist_id, :year
 
   def initialize(hash)
     @attributes = hash
+    @id = @attributes[:id]
+    @name = @attributes[:name]
+    @artist_id = @attributes[:artist_id]
+    @year = @attributes[:year]
   end
 end
--- test/photograph_test.rb
+++ test/photograph_test.rb
@@ -10,12 +10,21 @@ class PhotographTest < MiniTest::Test
   id: "1",
   name: "Rue Mouffetard, Paris (Boy with Bottles)",
   artist_id: 4,
-  year: 1954}
+  year: 1954
+}
     @photograph = Photograph.new(@attributes)
   end
 
   def test_photo_exists
     assert_instance_of Photograph, @photograph
+    #binding.pry
+  end
+
+  def test_photo_attributes
+    assert_equal "1", @photograph.id
+    assert_equal "Rue Mouffetard, Paris (Boy with Bottles)", @photograph.name
+    assert_equal 4, @photograph.artist_id
+    assert_equal 1954, @photograph.year
   end
 
 end
