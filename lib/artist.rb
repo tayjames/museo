58cb3e55bedc9f9934c21dcbba05e3ffe64e7a65
Tay <tayd93@hotmail.com>
2019-06-04 09:31:08 -0600

    Adds attributes

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,9 +1,13 @@
 require 'pry'
 
 class Artist
-  attr_reader
+  attr_reader :id, :name, :born, :died, :country
 
   def initialize(attributes)
-    
+    @id = attributes[:id]
+    @name = attributes[:name]
+    @born = attributes[:born]
+    @died = attributes[:died]
+    @country = attributes[:country]
   end
 end
--- test/artist_test.rb
+++ test/artist_test.rb
@@ -22,4 +22,12 @@ class ArtistTest < MiniTest::Test
     assert_instance_of Artist, @artist
   end
 
+  def test_artist_attributes
+    assert_equal "2", @artist.id
+    assert_equal "Ansel Adams", @artist.name
+    assert_equal "1902", @artist.born
+    assert_equal "1984", @artist.died
+    assert_equal "United States", @artist.country
+  end
+
 end
